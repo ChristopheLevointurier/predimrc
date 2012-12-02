db7b3c936413b45df648349f230615160fe93356
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-02 19:14:36 +0000

    new class for main frame: 3D view

---
+++ src/predimrc/gui/graphics/The3DView.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package predimrc.gui.graphics;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import predimrc.PredimRC;
+
+/**
+ *
+ * @author Christophe Levointurier 2 déc. 2012
+ * @version
+ * @see
+ * @since
+ */
+public class The3DView extends JPanel {
+
+    public The3DView() {
+        super();
+    }
+
+    public void showDraft() {
+        add(new JButton(PredimRC.getImageIcon("draft3DView.png")));
+    }
+}
