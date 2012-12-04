91f86442dca4beff8f2d6423a9d67d6bfed7ab74
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-04 19:38:42 +0000

    remove of useless wonstructor. Addes const usage

--- trunk/src/predimrc/gui/ExternalFrame.java
+++ trunk/src/predimrc/gui/ExternalFrame.java
@@ -4,6 +4,7 @@
  */
 package predimrc.gui;
 
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.AbstractButton;
@@ -21,26 +22,23 @@ import javax.swing.JFrame;
  */
 public class ExternalFrame extends JFrame {
 
-    private String title = "unknown";
-    private ImageIcon icon = null;
-    private int x = 800;
-    private int y = 600;
-    private AbstractButton caller;
+    protected String title = "unknown";
+    protected Image icon = null;
+    protected int x = predimrc.PredimRC.DEFAULT_X_FRAME;
+    protected int y = predimrc.PredimRC.DEFAULT_Y_FRAME;
+    protected AbstractButton caller;
 
-    public ExternalFrame(AbstractButton _caller, String _title, ImageIcon _icon) {
-        this(_caller, _title, _icon, 800, 600);
-    }
-
-    public ExternalFrame(AbstractButton _caller, String _title, ImageIcon _icon, int _x, int _y) {
-        super(_title);
+ 
+    public ExternalFrame(AbstractButton _caller, Image _icon, int _x, int _y) {
+        super();
         caller = _caller;
-        title = _title;
         icon = _icon;
         x = _x;
         y = _y;
         setSize(x, y);
         setResizable(true);
         setLocationRelativeTo(null);
+        setIconImage(icon);
         setVisible(true);
         setAlwaysOnTop(true);
         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
