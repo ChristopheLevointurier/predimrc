a153011f49f12d66bf0d7b73baca6d8d3c7268e2
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-04 19:37:21 +0000

    added content of configview

---
+++ trunk/src/predimrc/gui/graphic/ConfigView.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package predimrc.gui.graphic;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import predimrc.PredimRC;
+import predimrc.gui.MegaLabel;
+import predimrc.gui.frame.OptimFrame;
+import predimrc.gui.frame.VlmFrame;
+import predimrc.gui.frame.XFoilFrame;
+
+/**
+ *
+ * @author Christophe Levointurier, 2 déc. 2012
+ * @version
+ * @see
+ * @since
+ */
+public final class ConfigView extends JPanel {
+
+    private JPanel mainWing = new JPanel();
+    private JPanel mainWingButtons = new JPanel();
+    private JButton vlmBut = new JButton("VLM");
+    private JButton optimBut = new JButton("Optim");
+    private JButton xFoilBut = new JButton("xFoil");
+
+    public ConfigView() {
+        super();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        mainWing.setLayout(new BoxLayout(mainWing, BoxLayout.Y_AXIS));
+        mainWingButtons.setLayout(new BoxLayout(mainWingButtons, BoxLayout.X_AXIS));
+        mainWing.add(new JLabel("Main wing:"));
+        mainWing.add(new MegaLabel("wing span", "20", true));
+        mainWing.add(new MegaLabel("wing area", "100", true));
+        mainWing.add(new MegaLabel("airFoil", "trucmuch", true));
+        mainWing.add(new MegaLabel("aspect ratio", "x", true));
+        mainWing.add(new MegaLabel("corde moyenne", "xx", true));
+        mainWing.add(new MegaLabel("Alpha0", "xx", true));
+        mainWing.add(new MegaLabel("Cm0", "xx", true));
+        mainWingButtons.add(vlmBut);
+        mainWingButtons.add(optimBut);
+        mainWingButtons.add(xFoilBut);
+
+
+
+
+        vlmBut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new VlmFrame(vlmBut);
+            }
+        });
+
+
+        optimBut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new OptimFrame(optimBut);
+            }
+        });
+
+
+        xFoilBut.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new XFoilFrame(xFoilBut);
+            }
+        });
+
+
+        add(mainWing);
+        add(mainWingButtons);
+
+    }
+
+    public void showDraft() {
+        add(new JButton(PredimRC.getImageIcon("draftConfigView.png")));
+    }
+}
