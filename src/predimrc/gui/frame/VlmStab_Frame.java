6ce5687f734b302fc4d62f518d5a75e3009e7266
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-09 20:31:33 +0000

    new class for pop up

---
+++ src/predimrc/gui/frame/VlmStab_Frame.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package predimrc.gui.frame;
+
+import java.awt.Image;
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
+import predimrc.gui.ExternalFrame;
+
+/**
+ *
+ * @author Christophe Levointurier, 4 déc. 2012
+ * @version
+ * @see
+ * @since
+ */
+public class VlmStab_Frame extends ExternalFrame {
+
+    public VlmStab_Frame(AbstractButton _caller) {
+        this(_caller, predimrc.PredimRC.icon, predimrc.PredimRC.DEFAULT_X_FRAME, predimrc.PredimRC.DEFAULT_Y_FRAME);
+    }
+
+    public VlmStab_Frame(AbstractButton _caller, Image _icon, int _x, int _y) {
+        super(_caller, _icon, _x, _y);
+        title = "VlmStab";
+        setTitle(title);
+        getContentPane().add(new JTextArea("Zone de " + title));
+    }
+
+    @Override
+    public void save() {
+        predimrc.PredimRC.logDebugln("Save de " + title);
+    }
+}
