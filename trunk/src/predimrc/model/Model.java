f15cdf95ed887bf3112bc75225c90ca2db609b21
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-09 20:31:07 +0000

    new package for model. new class for I/O

---
+++ trunk/src/predimrc/model/Model.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package predimrc.model;
+
+import java.io.Serializable;
+
+/**
+ *  This class contains all model caracteristics for I/O
+ * @author Christophe Levointurier,  9 déc. 2012
+ * @version
+ * @see
+ * @since 
+ */
+public class Model implements Serializable{
+    
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+}
