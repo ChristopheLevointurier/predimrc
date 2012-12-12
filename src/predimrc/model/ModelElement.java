bf5de3b7e9799a8d4d8472f3354a1258f7d3ee2a
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-12 23:41:47 +0000

    classes for describing model

---
+++ src/predimrc/model/ModelElement.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package predimrc.model;
+
+/**
+ *
+ * @author Christophe Levointurier,  12 déc. 2012
+ * @version
+ * @see
+ * @since 
+ */
+public class ModelElement {
+
+}
---
+++ src/predimrc/model/element/Fuselage.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package predimrc.model.element;
+
+import predimrc.model.ModelElement;
+
+/**
+ *
+ * @author Christophe Levointurier,  12 déc. 2012
+ * @version
+ * @see
+ * @since 
+ */
+public class Fuselage extends ModelElement{
+
+}
