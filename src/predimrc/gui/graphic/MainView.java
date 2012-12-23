b86e48c41c680973d8cf0532be9becd4c58e0fce
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-23 17:17:11 +0000

    usage of dispacher

--- src/predimrc/gui/graphic/MainView.java
+++ src/predimrc/gui/graphic/MainView.java
@@ -5,17 +5,24 @@
 package predimrc.gui.graphic;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import predimrc.PredimRC;
-import predimrc.gui.IModelReact;
+import predimrc.controller.IModelListener;
 import predimrc.gui.MegaLabel;
 import predimrc.gui.graphic.drawable.CalagePanel;
 import predimrc.gui.graphic.drawable.DiedrePanel;
@@ -30,30 +37,37 @@ import predimrc.model.Model;
  * @see
  * @since
  */
-public class MainView extends JPanel implements MouseMotionListener, IModelReact {
+public class MainView extends JPanel implements MouseMotionListener, IModelListener {
 
     private MegaLabel modelTitle;
 
     public MainView() {
         super();
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        modelTitle = new MegaLabel("Model name:", "undefined", true);
+        modelTitle = new MegaLabel("Model name (Enter to validate):", "undefined", true);
+        modelTitle.addKeyListener("ENTER", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PredimRC.getInstance().getModel().setName(modelTitle.getValue());
+                modelTitle.setDefaultColor();
+            }
+        });
 
         modelTitle.addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                PredimRC.getInstance().getModel().setName(modelTitle.getValue());
+                modelTitle.setValueBackground(new Color(200, 150, 150));
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-             }
+                modelTitle.setValueBackground(new Color(200, 150, 150));
+            }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
             }
         });
-
 
 
 
@@ -85,5 +99,12 @@ public class MainView extends JPanel implements MouseMotionListener, IModelReact
     @Override
     public void changeModel(Model m) {
         modelTitle.setValue(m.getName());
+        modelTitle.setDefaultColor();
+
+    }
+
+    @Override
+    public void updateModel() {
+        changeModel(PredimRC.getInstance().getModel());
     }
 }
