76ede1b65bfdf4b363cc5b538e1adc14097688ee
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2012-12-26 02:11:05 +0000

    interface for drawable items

---
+++ src/predimrc/gui/graphic/drawable/IDrawableObject.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package predimrc.gui.graphic.drawable;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Chris
+ */
+public interface IDrawableObject {
+
+    public void draw(Graphics g);
+}
