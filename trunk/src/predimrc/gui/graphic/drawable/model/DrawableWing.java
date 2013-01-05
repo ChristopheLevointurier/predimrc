b629bda45ab7b8362af99bcb81318e01b76ec0fa
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-01-05 00:40:48 +0000

    new classes to describe drawableModel

---
+++ trunk/src/predimrc/gui/graphic/drawable/model/DrawableWing.java
@@ -0,0 +1,42 @@
+/*This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package predimrc.gui.graphic.drawable.model;
+
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import predimrc.gui.graphic.drawable.DrawablePoint;
+import predimrc.gui.graphic.drawable.DrawableWingPart;
+import predimrc.gui.graphic.drawable.IDrawableObject;
+import predimrc.model.element.Wing;
+
+/**
+ *
+ * @author Christophe Levointurier, 5 janv. 2013, (UTF-8)
+ * @version
+ * @see
+ * @since
+ */
+public class DrawableWing implements IDrawableObject {
+
+    private ArrayList<DrawableWingPart> wingParts = new ArrayList<>();
+    private DrawablePoint wingConnection = new DrawablePoint(100, 100);
+    private int indexWing = 0;
+    private Wing.USED_FOR usedFor; 
+
+    @Override
+    public void drawTop(Graphics2D g) {
+        //
+    }
+}
