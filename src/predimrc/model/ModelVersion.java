3ab06955344d29ca77479636a4012decbf784981
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-01-05 00:36:00 +0000

    remove of Tail class. Tail is now a list in Model, like Wing and Derive

--- src/predimrc/model/ModelVersion.java
+++ src/predimrc/model/ModelVersion.java
@@ -26,7 +26,7 @@ import java.io.Serializable;
  */
 public class ModelVersion implements Serializable {
 
-    public String VERSION_MODEL = "1.4";
+    public String VERSION_MODEL = "2.0";
 
     public ModelVersion() {
     }
