43de10f07bd69b83e1776f9a553e43a12ddb9839
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-01-16 21:05:35 +0000

    new model version: widthY,widthZ for fuselage

--- src/predimrc/model/ModelVersion.java
+++ src/predimrc/model/ModelVersion.java
@@ -26,7 +26,7 @@ import java.io.Serializable;
  */
 public class ModelVersion implements Serializable {
 
-    public String VERSION_MODEL = "3.48";
+    public String VERSION_MODEL = "3.5";
 
     public ModelVersion() {
     }
