f7e4a0a0cec21cb80425ef25bc4839aee18b28f8
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-02-10 21:04:51 +0000

    rem todo list

--- trunk/src/predimrc/PredimRC.java
+++ trunk/src/predimrc/PredimRC.java
@@ -66,20 +66,6 @@ import predimrc.model.Model;
 import predimrc.model.ModelVersion;
 
 /**
- * TODO .
- *
- *
- *
- * point de ref a installer calculs chaque voilure .
- *
- * corde = profondeur. * angle calculé par rapport à l'emplanture mettre
- * rectangle pour fuselage
- *
- *
- *
- *
- * sauver config 3D.
- *
  * @author Christophe Levointurier 11/2012
  * @version
  * @see
@@ -96,7 +82,7 @@ public class PredimRC extends JFrame {
     private static final String FILE_EXTENSION = "predimodel";
     final static float dash1[] = {10.0f};
     public final static BasicStroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f);
-    private static final String VERSION = "Alpha 0.55";
+    private static final String VERSION = "Alpha 0.56";
     private static final long serialVersionUID = -2615396482200960443L;    // private final static String saveFileName = "links.txt";
     public static final String appRep = System.getProperty("user.home") + "\\PredimRCFiles\\";
     public static final String modelRep = System.getProperty("user.home") + "\\PredimRCFiles\\models\\";
