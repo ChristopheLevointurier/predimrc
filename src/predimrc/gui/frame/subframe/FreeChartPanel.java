d6802f04bcbcf8ef69263363d00e5de528ddf5b5
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-03-19 23:02:01 +0000

    new classes to render charts

---
+++ src/predimrc/gui/frame/subframe/FreeChartPanel.java
@@ -0,0 +1,71 @@
+/*This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package predimrc.gui.frame.subframe;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import predimrc.gui.graphic.drawable.tool.DrawablePoint;
+
+/**
+ *
+ * @author Christophe Levointurier, 19 mars 2013, (UTF-8)
+ * @version
+ * @see
+ * @since
+ */
+public class FreeChartPanel extends JPanel {
+
+    private XYSeriesCollection xyseriescollection = new XYSeriesCollection();
+    private XYLineAndShapeRenderer xylineandshaperenderer;
+    private String x, y, title;
+    private ChartPanel chartPanel;
+
+    public FreeChartPanel(String _title, String _x, String _y) {
+        super();
+        title = _title;
+        x = _x;
+        y = _y;
+        updateChart();
+    }
+
+    private void updateChart() {
+        removeAll();
+        chartPanel = new ChartPanel(ChartFactory.createXYLineChart(title, x, y, xyseriescollection, PlotOrientation.VERTICAL, false, true, false));
+        xylineandshaperenderer = (XYLineAndShapeRenderer) chartPanel.getChart().getXYPlot().getRenderer();
+        chartPanel.setMouseWheelEnabled(true);
+        chartPanel.setPreferredSize(new Dimension(480, 400));
+        chartPanel.setMouseWheelEnabled(true);
+        add(chartPanel);
+        chartPanel.repaint();
+    }
+
+    public void addSeries(Color col, ArrayList<DrawablePoint> l) {
+        XYSeries series = new XYSeries(xyseriescollection.getSeriesCount());
+        for (DrawablePoint p : l) {
+            series.add(p.getFloatX(), p.getFloatY());
+        }
+        xyseriescollection.addSeries(series);
+        xylineandshaperenderer.setSeriesPaint(0, col);
+        chartPanel.repaint();
+    }
+}
---
+++ src/predimrc/gui/frame/subframe/PolardataLine.java
@@ -0,0 +1,76 @@
+/*This program is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU General Public License
+ *as published by the Free Software Foundation; either version 2
+ *of the License, or (at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package predimrc.gui.frame.subframe;
+
+/**
+ * A line of data from polars.dat
+ *
+ * @author Christophe Levointurier, 19 mars 2013, (UTF-8)
+ * @version
+ * @see
+ * @since
+ */
+public class PolardataLine {
+
+    private float alpha = 0f;
+    private float Cz = 0f;     //CL   
+    private float Cx = 0f;    //CD 
+    private float CDp = 0f;
+    private float Cm = 0f;
+    private float Top_Xtr = 0f;
+    private float Bot_Xtr = 0f;
+
+    public PolardataLine(String alpha, String CZ, String Cx, String CDp, String CM, String Top_Xtr, String Bot_Xtr) {
+        try {
+            this.alpha = Float.parseFloat(alpha);
+            this.Cz = Float.parseFloat(CZ);
+            this.Cx = Float.parseFloat(Cx);
+            this.CDp = Float.parseFloat(CDp);
+            this.Cm = Float.parseFloat(CM);
+            this.Top_Xtr = Float.parseFloat(Top_Xtr);
+            this.Bot_Xtr = Float.parseFloat(Bot_Xtr);
+        } catch (java.lang.NumberFormatException e) {
+            predimrc.PredimRC.logln("NumberFormatException:" + alpha + ":" + CZ + ":" + Cx + ":" + CDp + ":" + CM + ":" + Top_Xtr + ":" + Bot_Xtr);
+        }
+    }
+
+    public float getAlpha() {
+        return alpha;
+    }
+
+    public float getCz() {
+        return Cz;
+    }
+
+    public float getCx() {
+        return Cx;
+    }
+
+    public float getCDp() {
+        return CDp;
+    }
+
+    public float getCm() {
+        return Cm;
+    }
+
+    public float getTop_Xtr() {
+        return Top_Xtr;
+    }
+
+    public float getBot_Xtr() {
+        return Bot_Xtr;
+    }
+}
