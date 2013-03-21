427b136a3f85b9d4b52f9062a0860fce2c63ea4f
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-03-21 22:53:32 +0000

    added one reynolds value

--- trunk/src/predimrc/model/ModelVersion.java
+++ trunk/src/predimrc/model/ModelVersion.java
@@ -26,7 +26,7 @@ import java.io.Serializable;
  */
 public class ModelVersion implements Serializable {
 
-    public String VERSION_MODEL = "3.73";
+    public String VERSION_MODEL = "3.74";
 
     public ModelVersion() {
     }
