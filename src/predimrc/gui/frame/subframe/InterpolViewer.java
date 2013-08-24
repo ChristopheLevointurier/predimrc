273bbc0507f61870a10acf0e0407285b046c2ea8
levointurier@gmail.com <levointurier@gmail.com@c531b4e3-1bad-7196-0ab3-b8a91e6fc541>
2013-08-24 09:24:54 +0000

    added data in chart. ready to compute

--- src/predimrc/gui/frame/subframe/InterpolViewer.java
+++ src/predimrc/gui/frame/subframe/InterpolViewer.java
@@ -16,14 +16,21 @@ package predimrc.gui.frame.subframe;
 
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import org.jfree.data.xy.XYSeriesCollection;
 import predimrc.PredimRC;
+import predimrc.data.PolarData;
+import predimrc.data.PolarDataBase;
+import predimrc.data.PolarKey;
 import predimrc.gui.MiniFrame;
+import predimrc.gui.frame.XFoil_Frame;
 import predimrc.gui.frame.subframe.panel.FreeChartPanel;
+import predimrc.model.element.XfoilConfig;
 
 /**
  *
@@ -33,47 +40,56 @@ import predimrc.gui.frame.subframe.panel.FreeChartPanel;
  * @since
  */
 public class InterpolViewer extends MiniFrame {
-    
-    private static FreeChartPanel chart;
+
+    private static FreeChartPanel chart = new FreeChartPanel("", "Cx", "Cz", new XYSeriesCollection());
     private static JSlider slider = new JSlider();
     private static boolean init = false;
-    
-    
+    private static PolarData data1;
+    private static PolarData data2;
     private static ChangeListener slide = new ChangeListener() {
         @Override
         public void stateChanged(ChangeEvent e) {
             PredimRC.logln("slider value:" + slider.getValue());
         }
     };
-    
+
     public static void initViewer() {
         if (init) {
             return;
         }
-        //computation of the interpolation
-        //chart init
+        XfoilConfig conf = PredimRC.getInstanceDrawableModel().getXfoilConfig();
+        if (conf.getConfigs().size() < 2) {
+            chart.getChart().setTitle("XfoilConfig has less than 2 data to interpolate");
+            return;
+        }
+        data1 = PolarDataBase.getPolar(new PolarKey(conf.getConfigs().get(0)), true);
+        data2 = PolarDataBase.getPolar(new PolarKey(conf.getConfigs().get(1)), true);
+
+        chart.addSeries(XFoil_Frame.getAppropriateColor(data1.getColIndex(), ReynoldsConfig.getConfig().size() - 1), data1.getCzCxData());
+        chart.addSeries(XFoil_Frame.getAppropriateColor(data2.getColIndex(), ReynoldsConfig.getConfig().size() - 1), data2.getCzCxData());
+        //TODO computation of the interpolation
         init = true;
     }
-    
+
     public InterpolViewer(AbstractButton _caller, JFrame _mother) {
         super(_caller, _mother);
         title = "Interpolation Viewer";
         setTitle(title);
         JPanel content = new JPanel();
-        //  content.setLayout(new BoxLayout(content, BoxLayout.X_AXIS));
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
         content.setBorder(BorderFactory.createTitledBorder(BorderFactory.createRaisedBevelBorder(), "Select Reynold"));
         // chart.setLayout(new BoxLayout(chart, BoxLayout.Y_AXIS));
-        
+
         slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
         slider.addChangeListener(slide);
         slider.setMajorTickSpacing(10);
         slider.setMinorTickSpacing(1);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-              
+        initViewer();
+        content.add(chart);
         content.add(slider);
-     //   content.add(chart);
         getContentPane().add(content);
-        pack();     
+        pack();
     }
 }
